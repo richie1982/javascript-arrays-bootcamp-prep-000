6c1ff2f1c28dd8df36da7cb783cf05c06ce33e8d
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-05 14:48:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -36,5 +36,6 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars){
   return chocolateBars;
   }
 function removeElementFromEndOfArray(chocolateBars){
-  
+  chocolateBars.slice(0, chocolateBars.length - 1);
+  return chocolateBars;
 }
