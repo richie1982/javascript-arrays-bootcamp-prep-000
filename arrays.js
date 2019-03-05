5eba6af5283cd45ae6f89004042a55f4815bcee7
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-05 13:53:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function 
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
+}
