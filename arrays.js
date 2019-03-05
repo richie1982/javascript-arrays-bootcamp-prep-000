372b544f43de9aeef50504916192b9e494ff9311
Richard Dowland <rdowland@hotmail.co.uk>
2019-03-05 13:59:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,15 +2,18 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(chocolateBars, candy){
   return [candy, ...chocolateBars];
-}
+};
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, candy){
   chocolateBars.unshift(candy)
   return chocolateBars
-}
+};
 
 function addElementToEndOfArray(chocolateBars, candy){
   return [...chocolateBars, candy]
-}
+};
 
-function 
+function destructivelyAddElementToEndOfArray(chocolateBars, candy){
+  chocolateBars.push(candy)
+  return chocolateBars
+};
